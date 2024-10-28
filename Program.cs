53ca02ef8a327971b2d2f1f25efe2b0d743bbc8f
nightoul <jakub.jenc@gmail.com>
2024-10-28 15:06:45 +0100

    port

--- Program.cs
+++ Program.cs
@@ -3,6 +3,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080"; // Fallback to 8080 if not set
+builder.WebHost.UseKestrel()
+    .ConfigureKestrel(serverOptions =>
+    {
+        serverOptions.ListenAnyIP(int.Parse(port)); // Listen on 0.0.0.0
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
